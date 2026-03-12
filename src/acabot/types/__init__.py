7df8a00a26652d3eaad9487d37e9aebe062bc61b
vacacia <yingjiefu@ustc.mail.edu.cn>
2026-03-12 17:44:35 +0800

    refactor(runtime): remove legacy pipeline and session stack

--- src/acabot/types/__init__.py
+++ src/acabot/types/__init__.py
@@ -1,9 +1,7 @@
 from .event import StandardEvent, EventSource, MsgSegment, EventAttachment, ReplyReference
 from .action import Action, ActionType
-from .hook import HookPoint, HookResult, HookContext
 
 __all__ = [
     "StandardEvent", "EventSource", "MsgSegment", "EventAttachment", "ReplyReference",
     "Action", "ActionType",
-    "HookPoint", "HookResult", "HookContext",
 ]
